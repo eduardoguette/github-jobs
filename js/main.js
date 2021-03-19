1a4321e91ac327757647fa5cfe7766bee10be63c
Eduardo <eduardoguette@gmail.com>
2021-03-19 11:59:56 +0100

    add spinner

--- js/main.js
+++ js/main.js
@@ -120,7 +120,7 @@ function mostrarJobs(jobs) {
   } else {
     htmlJobs.classList.add('no-results')
     htmlJobs.innerHTML = `
-     <img src="https://media2.giphy.com/media/V0E9fl9dPKWWI/giphy.gif?cid=ecf05e47ld9aubiebvneh5zb14uo3l1u5t9th0x4y9hqusdb&rid=giphy.gif" alt="no more jobs image"/>
+     <img width="300" src="https://media2.giphy.com/media/V0E9fl9dPKWWI/giphy.gif?cid=ecf05e47ld9aubiebvneh5zb14uo3l1u5t9th0x4y9hqusdb&rid=giphy.gif" alt="no more jobs image"/>
      <p>No results</p>
     `
     if(document.querySelector('.spinner'))
